0d4ccbbfd8e3cbd2edc862e3f2ce187a164fd1bf
sapanavin <116043155+sapanavin@users.noreply.github.com>
2023-10-03 16:11:27 -0500

    configuring Okta for Authentication and Authorization

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,7 +4,6 @@ import { OktaAuth } from '@okta/okta-auth-js';
 import OktaSignIn from '@okta/okta-signin-widget';
 
 import myAppConfig from '../../config/my-app-config';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,18 +13,44 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   oktaSignin: any;
-  router:Router;
 
-  constructor(router:Router) {
-    this.router=router;
-  }
-   ngOnInit() {
-    
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+   }
+
+  ngOnInit(): void {
+console.log(this.oktaSignin);
+    console.log("from ngOnInit in okta login component");
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({ 
+
+      el: '#okta-sign-in-widget'}, // this name should be same as div tag id in login.component.html
+      
+      (response: any) => {
+        if (response.status === 'SUCCESS') {
+          console.log("from response in okta login component",response);
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error: any) => {
+        console.log("from errroe in okta login component",error);
+        throw error;
+      }
+    );
+    console.log("from oktaSignin.renderEl in okta login component");
+    console.log(this.oktaSignin);
   }
 
-  onClickSubmit(form: any):void{
-    this.router.navigateByUrl('/members');
-    console.log("From submitting the form ----------------",form)
-
-  }
 }
