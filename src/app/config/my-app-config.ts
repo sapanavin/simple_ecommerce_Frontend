2b54c1ed931d5dde57c348555d62944ea4c312aa
sapanavin <116043155+sapanavin@users.noreply.github.com>
2023-10-04 22:07:09 -0500

    working on Okta - 4

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa6u9htmzJUqxIyP5d7',
         issuer: 'https://dev-37342367.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://simpleecommerce-2aa1a.web.app/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
