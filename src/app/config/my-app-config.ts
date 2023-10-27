cd6272a2adddc796d1617d338d95f08d432f1afe
sapanavin <116043155+sapanavin@users.noreply.github.com>
2023-10-27 13:48:02 -0500

    NgxUi Loader for all  HTTP Request is implemented-2

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,8 +3,8 @@ export default {
     oidc: {
         clientId: '0oa6u9htmzJUqxIyP5d7',
         issuer: 'https://dev-37342367.okta.com/oauth2/default',
-       //redirectUri: 'https://simpleecommerce-2aa1a.web.app/login/callback',
-        redirectUri: 'http://localhost:4200/login/callback',//for local enviorment
+       redirectUri: 'https://simpleecommerce-2aa1a.web.app/login/callback',//for Production Enviorment
+        //redirectUri: 'http://localhost:4200/login/callback',//for local enviorment
         scopes: ['openid', 'profile', 'email']
     }
 
